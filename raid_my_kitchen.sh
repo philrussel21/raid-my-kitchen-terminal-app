13ce7b816fb6ac2b3b8003926146adf1ecd470af
philrussel21 <phil.antiporda21@gmail.com>
2020-04-27 11:14:56 +1000

    Creates a exe sh file to install gems and run the app for the first time

---
+++ raid_my_kitchen.sh
@@ -0,0 +1,5 @@
+echo "Welcome to **TERMINAL APP NAME**"
+echo "Time to ensure all required gems are installed"
+bundle install
+echo "All gems have been installed, running application for the first time now"
+ruby init.rb
